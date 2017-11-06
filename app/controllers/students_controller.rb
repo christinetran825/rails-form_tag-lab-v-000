704db410607b063bad7323eca79dfa8938dc50f2
Christine Tran <christinetran825@gmail.com>
2017-11-06 04:53:21 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,6 @@ class StudentsController < ApplicationController
   end
 
   def new
-    @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
   end
 
   def create
